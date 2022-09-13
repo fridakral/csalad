5b4790c6ef20c8f9c491aaec7c04dc93dfb4c402
fridakral <64780911+fridakral@users.noreply.github.com>
2022-09-13 16:31:11 +0200

    fishing oldal elkezdve

---
+++ andris/navigate.js
@@ -0,0 +1,7 @@
+function navigateToFishing(){
+    window.location="https://csalad.kral.hu/andris/fishing";
+}
+
+function navigateToPaintball(){
+    window.location="https://csalad.kral.hu/andris/paintball";
+}
