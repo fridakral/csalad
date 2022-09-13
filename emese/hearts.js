29573902b090dcc18a02610ca9085bb5c6779190
fridakral <64780911+fridakral@users.noreply.github.com>
2022-09-13 10:34:43 +0200

    emese szerelmeslevél oldal

---
+++ emese/hearts.js
@@ -0,0 +1,17 @@
+function hearts(){
+    const container = document.querySelector('.container');
+    const create = document.createElement('div');
+    create.classList.add('hearts');
+    create.innerHTML='❤';
+    create.style.left=Math.random() * 100 + 'vw';
+    create.style.animationDuration=Math.random()* 3 + 2 + 's';
+    container.appendChild(create);
+
+    setTimeout(()=>{
+        create.remove();
+    }, 3000);
+}
+
+hearts();
+
+setInterval(hearts, 100);
